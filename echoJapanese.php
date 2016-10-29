115cca76294de531035b0c2ccc15e5f7b0a22278
Yuki Yoshida <yi-yoshida@kddi.com>
2016-10-29 16:50:38 +0900

    returnかを追加

--- echoJapanese.php
+++ echoJapanese.php
@@ -6,10 +6,15 @@ function execute() {
     $return = "";
 
     $return .= returnあ();
+    $return .= returnか()
 
     return $return;
 }
 
 function returnあ() {
     return "あ";
+}
+
+function returnか() {
+    return "か";
 }
